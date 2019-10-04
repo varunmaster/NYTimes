93adc6f2c3d5e1f360cd52e823c3539f0ce54390
Varun Master <varunmaster95@gmail.com>
2019-10-04 16:01:21 -0400

    added the form inside the div and added empty() to clear out the page when searching new topics

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     $("#search").on("click", function (event) {
         event.preventDefault();
+        $("#result").empty();
         var apiKey = "Q78GxHgh9EcePRh7nm5Gs7Om6v1Tp5EH";
         var userQuery = $("#searchBarId").val().trim();
         var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + userQuery;
 
-        console.log("user term: ", userQuery);
-        console.log("url: ", queryUrl);
+        //console.log("user term: ", userQuery);
+        //console.log("url: ", queryUrl);
         
         $.ajax({
             url: queryUrl,
@@ -14,16 +15,16 @@ $(document).ready(function () {
         }).then(function (res) {
             console.log(res);
             var result = res.response;
-            console.log(result);
+            //console.log(result);
             for (var i = 0; i < result.docs.length; i++) {
                 var articleDiv = $("<div>");
                 articleDiv.attr("class", "card-body"); 
                 var title = $("<h3>");
                 title.text(i+1 + ". " + result.docs[i].headline.main);
-                console.log("title: ",result.docs[i].headline.main);
+                //console.log("title: ",result.docs[i].headline.main);
                 var author = $("<h2>");
                 author.text(result.docs[i].byline.original);
-                console.log("author: ",result.docs[i].byline.original);
+                //console.log("author: ",result.docs[i].byline.original);
                 var section = $("<h2>");
                 section.text("Section: " + result.docs[i].section_name);
                 var pubDate = $("<h2>");
